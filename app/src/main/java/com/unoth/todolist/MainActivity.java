93559cbcb71c145bf4495aa8fa60bb884ce4226c
Kristina Dedova <dan005bk@gmail.com>
2023-04-20 02:05:45 +0300

    created Adapter

--- app/src/main/java/com/unoth/todolist/MainActivity.java
+++ app/src/main/java/com/unoth/todolist/MainActivity.java
@@ -7,6 +7,8 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.unoth.todolost.R;
 import java.util.ArrayList;
@@ -14,9 +16,10 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    private LinearLayout linearLayoutNotes;
+    private RecyclerView recyclerView;
     private FloatingActionButton btnAddNote;
     private Database database = Database.getInstance();
+    private NotesAdapter notesAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +27,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         initViews();
 
+        notesAdapter = new NotesAdapter();
+        recyclerView.setAdapter(notesAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
         btnAddNote.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -40,31 +46,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initViews() {
-        linearLayoutNotes = findViewById(R.id.linearLayoutNotes);
+        recyclerView = findViewById(R.id.rcNote);
         btnAddNote = findViewById(R.id.btnAddNote);
     }
 
     private void showNotes() {
-        linearLayoutNotes.removeAllViews();
-        for (Note note : database.getNotes()) {
-            View view = getLayoutInflater().inflate(R.layout.note_item, linearLayoutNotes, false);
-            TextView textViewNote = view.findViewById(R.id.noteItem);
-            textViewNote.setText(note.getText());
-            int colorId;
-            switch (note.getPriority()) {
-                case 0:
-                    colorId = android.R.color.holo_green_light;
-                    break;
-                case 1:
-                    colorId = android.R.color.holo_orange_light;
-                    break;
-                default:
-                    colorId = android.R.color.holo_red_light;
-            }
-            int color = ContextCompat.getColor(this,colorId);
-            textViewNote.setBackgroundColor(color);
-            linearLayoutNotes.addView(view);
-
-        }
+        notesAdapter.setNotes(database.getNotes());
     }
 }
---
+++ app/src/main/java/com/unoth/todolist/NotesAdapter.java
@@ -0,0 +1,66 @@
+package com.unoth.todolist;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
+import com.unoth.todolost.R;
+import java.util.ArrayList;
+
+public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.NoteViewHolder> {
+
+    private ArrayList<Note> notes = new ArrayList<>();
+
+    public void setNotes(ArrayList<Note> notes) {
+        this.notes = notes;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public NoteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from((parent).getContext()).inflate(
+                R.layout.note_item,
+                parent,
+                false
+        );
+        return new NoteViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(NoteViewHolder viewHolder, int position) {
+        Note note = notes.get(position);
+        viewHolder.textViewNote.setText(note.getText());
+        int colorId;
+        switch (note.getPriority()) {
+            case 0:
+                colorId = android.R.color.holo_green_light;
+                break;
+            case 1:
+                colorId = android.R.color.holo_orange_light;
+                break;
+            default:
+                colorId = android.R.color.holo_red_light;
+        }
+        int color = ContextCompat.getColor(viewHolder.itemView.getContext(), colorId);
+        viewHolder.textViewNote.setBackgroundColor(color);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+    class NoteViewHolder extends RecyclerView.ViewHolder {
+        TextView textViewNote;
+
+        public NoteViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewNote = itemView.findViewById(R.id.noteItem);
+        }
+    }
+}
