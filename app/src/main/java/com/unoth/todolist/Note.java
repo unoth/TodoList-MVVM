760e9b7e7ffd24eec07eeb8841460d5e108b5014
Kristina Dedova <dan005bk@gmail.com>
2023-04-24 22:23:16 +0300

    added Room database

--- app/src/main/java/com/unoth/todolist/Note.java
+++ app/src/main/java/com/unoth/todolist/Note.java
@@ -1,6 +1,11 @@
 package com.unoth.todolist;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "notes")
 public class Note {
+    @PrimaryKey(autoGenerate = true)
     private int id;
     private String text;
     private int priority;
---
+++ app/src/main/java/com/unoth/todolist/NoteDatabase.java
@@ -0,0 +1,23 @@
+package com.unoth.todolist;
+
+import android.app.Application;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Note.class}, version = 1)
+public abstract class NoteDatabase extends RoomDatabase {
+
+    private static NoteDatabase instance = null;
+
+    private static NoteDatabase getInstance(Application application) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(
+                    application,
+                    NoteDatabase.class,
+                    "note.bd"
+            ).build();
+        }
+        return instance;
+    }
+}
