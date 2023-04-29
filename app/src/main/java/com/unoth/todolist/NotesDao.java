b1899d113bfdfcc8c3dc14e7db7a18c7fcfbf1aa
Kristina Dedova <dan005bk@gmail.com>
2023-04-29 14:20:28 +0300

    added Room database

---
+++ app/src/main/java/com/unoth/todolist/NotesDao.java
@@ -0,0 +1,19 @@
+package com.unoth.todolist;
+
+import androidx.room.Dao;
+import androidx.room.Entity;
+import androidx.room.Insert;
+import androidx.room.Query;
+import java.util.ArrayList;
+
+@Dao
+public interface NotesDao {
+    @Query("SELECT * FROM notes")
+    ArrayList<Note> getNote();
+
+    @Insert
+    void add(Note note);
+
+    @Query("DELETE FROM notes WHERE id = :id")
+    void remove(int id);
+}
