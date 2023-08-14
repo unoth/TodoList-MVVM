53d8df17582353a48f0fe9c22e3e4fb0695200ce
Kristina Dedova <dan005bk@gmail.com>
2023-08-14 19:59:29 +0300

    using RxJava

--- app/src/main/java/com/unoth/todolist/AddNoteViewModel.java
+++ app/src/main/java/com/unoth/todolist/AddNoteViewModel.java
@@ -1,18 +1,23 @@
 package com.unoth.todolist;
 
 import android.app.Application;
+import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.functions.Action;
 import io.reactivex.rxjava3.schedulers.Schedulers;
+import java.util.concurrent.TimeUnit;
 
 public class AddNoteViewModel extends AndroidViewModel {
 
     private NotesDao notesDao;
     private MutableLiveData<Boolean> shouldCloseScreen = new MutableLiveData<>();
+    private CompositeDisposable compositeDisposable = new CompositeDisposable();
 
     public AddNoteViewModel(@NonNull Application application) {
         super(application);
@@ -24,14 +29,23 @@ public class AddNoteViewModel extends AndroidViewModel {
     }
 
     public void addNote(Note note) {
-        notesDao.add(note)
+        Disposable disposable = notesDao.add(note)
+                .delay(5, TimeUnit.SECONDS)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Action() {
                     @Override
                     public void run() throws Throwable {
+                        Log.d("AddNoteViewModel", "subscribe OK");
                         shouldCloseScreen.setValue(true);
                     }
                 });
+        compositeDisposable.add(disposable);
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        compositeDisposable.dispose();
     }
 }
